6fb27114f56dc4627834b4d88910ce7d6c058dbd
João Felipe Zini <joaofelipezini@gmail.com>
2023-08-25 19:20:02 -0300

    creating architecture

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
 const express = require('express');
 
-// ...
-
 const app = express();
 
-// não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
 });
 
 app.use(express.json());
 
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
